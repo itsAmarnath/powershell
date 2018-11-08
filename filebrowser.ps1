427979fcc7bbf46f8846615b0453fa7d2b67ea0b
itsAmarnath <40494037+itsAmarnath@users.noreply.github.com>
2018-11-08 21:02:02 +0000

    Add files via upload

---
+++ filebrowser.ps1
@@ -0,0 +1,5 @@
+﻿$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog #-Property @{ 
+    #InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    #Filter = 'Documents (*.docx)|*.docx|SpreadSheet (*.xlsx)|*.xlsx'
+#}
+$null = $FileBrowser.ShowDialog()
